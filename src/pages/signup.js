e6fac7ffcca8421f78865c70f7be62aa9ba968c0
Achraf <achraf.rehioui.1@gmail.com>
2020-11-23 11:33:57 +0100

    Modifying handleSignUp for creating User with email and password

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -21,13 +21,20 @@ export default function Signup() {
     const handleSignUp = (event) => {
         event.preventDefault();
 
-        return firebase
+        firebase
             .auth()
-            .signInWithEmailAndPassword(emailAddress, password)
-            .then(() => {
-                history.push(ROUTES.BROWSE);
+            .createUserWithEmailAndPassword(emailAddress, password)
+            .then((result) => {
+                result.user.updateProfile({
+                    displayName: firstName,
+                    photoURL: Math.floor(Math.random() * 5) + 1,
+                })
+                    .then(() => {
+                        history.push(ROUTES.BROWSE);
+                    })
             })
             .catch((error) => {
+                setFirstName('');
                 setEmailAddress('');
                 setPassword('');
                 setError(error.message);
