fdd9f3a02919bd4f3a4d227ac53cabecf4c9ebd4
Achraf <achraf.rehioui.1@gmail.com>
2020-11-29 01:30:04 +0100

    Testing Player

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -20,7 +20,7 @@ Player.Video = function PlayerVideo({ src, ...restProps }) {
     const { showPlayer, setShowPlayer } = useContext(PlayerContext);
 
     return showPlayer ? ReactDOM.createPortal(
-        <Overlay onClick={() => setShowPlayer(false)}>
+        <Overlay onClick={() => setShowPlayer(false)} {...restProps} data-testid="player">
             <Inner>
                 <video id="netflix-player" controls>
                     <source src={src} type="video/mp4" />
