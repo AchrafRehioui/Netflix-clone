1cecac3482cd9b59bb496e58234aab0e8a308873
Achraf <achraf.rehioui.1@gmail.com>
2020-11-30 21:20:41 +0100

    testing header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -21,7 +21,7 @@ import {
 
 
 export default function Header({ bg = true, children, ...restProps }) {
-    return bg ? (<Background {...restProps}>{children}</Background>) : (children);
+    return bg ? (<Background {...restProps} data-testid="header-bg">{children}</Background>) : (children);
 }
 
 Header.Feature = function HeaderFeature({ children, ...restProps }) {
@@ -45,7 +45,7 @@ Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps 
 
     return (
         <Search {...restProps}>
-            <SearchIcon onClick={() => setSearchActive((searchActive) => !searchActive)} >
+            <SearchIcon onClick={() => setSearchActive((searchActive) => !searchActive)} data-testid="search-click">
                 <img src="/images/icons/search.png" alt="Search" />
             </SearchIcon>
             <SearchInput
